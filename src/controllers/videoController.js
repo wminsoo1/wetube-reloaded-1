32d116df19eb405c0ef2ab2804cf5a12ea208f37
wminsoo1 <wminsoo1@naver.com>
2023-01-18 21:02:23 +0900

    partials

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,8 @@
 export const trending = (req, res) => res.render("home");
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("upload");
-export const see = (req, res) => {
-  return res.send(`Watch Video #${req.params.id}`);
-};
-export const edit = (req, res) => {
-  return res.send("Edit");
-};
 export const deleteVideo = (req, res) => {
   return res.send("deletevideo");
 };
