2613b3884339f23d00fa5aff4988b691f694fb2c
wminsoo1 <wminsoo1@naver.com>
2023-01-18 19:38:04 +0900

    parameters(:) one

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,9 +3,9 @@ import { see, edit, deleteVideo, upload } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
+videoRouter.get("/upload", upload);
 videoRouter.get("/:id", see);
 videoRouter.get("/:id/edit", edit);
 videoRouter.get("/:id/delete", deleteVideo);
-videoRouter.get("/upload", upload);
 
 export default videoRouter;
