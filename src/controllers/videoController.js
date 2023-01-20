cfe29b39e95ba2d6b5bd9a927cfd4acb4a660991
wminsoo1 <wminsoo1@naver.com>
2023-01-20 16:44:54 +0900

    Update videoController.js

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -30,20 +30,16 @@ export const trending = (req, res) => {
 export const watch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
 };
-export const edit = (req, res) => res.render("edit");
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("upload");
-export const deleteVideo = (req, res) => {
-  return res.send("deletevideo");
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
 };
-<<<<<<< HEAD
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
   videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
-=======
->>>>>>> parent of 1e367a5 (edit video 1)
