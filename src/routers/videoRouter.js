2143fd7b98faeb791990493e3f4bc6d83b9a4a62
wminsoo1 <wminsoo1@naver.com>
2023-01-20 16:50:56 +0900

    Update videoRouter.js

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,20 +1,9 @@
 import express from "express";
-import {
-  watch,
-  edit,
-  deleteVideo,
-  upload,
-} from "../controllers/videoController";
+import { watch, getEdit, postEdit } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", watch);
-<<<<<<< HEAD
+videoRouter.route("/:id(\\d+)").get(watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
-=======
-videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
->>>>>>> parent of 1e367a5 (edit video 1)
 
 export default videoRouter;
