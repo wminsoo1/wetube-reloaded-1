9152d4298eb002a5a7cd9977253f4e85aa86ef9c
wminsoo1 <wminsoo1@naver.com>
2023-01-20 21:55:47 +0900

    our first query

---
+++ src/init.js
@@ -0,0 +1,10 @@
+import "./db";
+import "./models/Video";
+import app from "./server";
+
+const PORT = 4000;
+
+const handelListening = () =>
+  console.log(`Server listenting on port http://localhost:${PORT}`);
+
+app.listen(PORT, handelListening);
