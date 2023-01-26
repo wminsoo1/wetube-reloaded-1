d5616c5fb4571818fb99b7aa12c5d3bd3ce1d3fe
wminsoo1 <wminsoo1@naver.com>
2023-01-26 18:43:31 +0900

    async await
    수행될 때까지 기다려준다(await)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,8 @@
 import Video from "../models/Video";
 
-export const home = (req, res) => {
-  console.log("Start");
-  Video.find({}, (error, videos) => {
-    console.log("Finished ");
-    return res.render("home", { pageTitle: "Home", videos: [] });
-  });
-  console.log("I finish first");
+export const home = async (req, res) => {
+  const videos = await Video.find({});
+  return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => {
   const { id } = req.params;
