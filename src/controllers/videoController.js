0ab69432a3ba94b5208befbd0c5cb2a6daa52919
wminsoo1 <wminsoo1@naver.com>
2023-01-26 19:56:19 +0900

    creating a video 1
    new Video부분은 Video.js 모양이랑 같게 만들어야 함

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -21,7 +21,17 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 export const postUpload = (req, res) => {
-  const { title } = req.body;
-  videos.push(newVideo);
+  const { title, description, hashtags } = req.body;
+  const video = new Video({
+    title,
+    description,
+    createdAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `#${word}`),
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+  });
+  console.log(video);
   return res.redirect("/");
 };
