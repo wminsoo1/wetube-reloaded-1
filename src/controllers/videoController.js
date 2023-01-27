9f77bd3efb796428698a76ebb0653eebc8b55d5b
wminsoo1 <wminsoo1@naver.com>
2023-01-27 21:25:05 +0900

    middlewares
    1. middleware는 무조건 model이 생성되기 전에 만들어야 한다
    2. mongoose의 middleware에서 this키워드는 저장하고자 하는 문서를 가리킴
    3. middleware에서 해시태그 관리 구현

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,7 +2,6 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   const videos = await Video.find({});
-  console.log(videos);
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = async (req, res) => {
@@ -48,7 +47,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags.split(",").map((word) => `#${word}`),
+      hashtags,
     });
     return res.redirect("/");
   } catch (error) {
