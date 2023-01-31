09693d8c184d38b68b39c8df4ae9af89671e7487
wminsoo1 <wminsoo1@naver.com>
2023-01-31 18:28:27 +0900

    create account 2

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,17 @@
+import User from "../models/User";
+
 export const getjoin = (req, res) => res.render("join", { pageTitle: "Join" });
-export const postjoin = (req, res) => {
+export const postjoin = async (req, res) => {
   console.log(req.body);
-  res.end();
+  const { email, username, password, name, location } = req.body;
+  await User.create({
+    email,
+    username,
+    password,
+    name,
+    location,
+  });
+  return res.redirect("/login");
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
