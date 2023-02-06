04791c8f0ef2f32cfd46293de5db5096c5680b40
wminsoo1 <wminsoo1@naver.com>
2023-02-06 17:24:06 +0900

    github login part 3
    1. fetch('url')로 다른 서버를 통해 데이터를 가져올 수있다.
    하지만, res.body 에 담겨있는 날것의 url로는 제대로 된 객체를 받아올 수 없다.
    
    2.때문에 중간에 .json 함수가 response의 스트림을 가져와 끝까지 읽고, res.body의 텍스트를 promise의 형태로 반환한다.
    
    3. 다른 서버에서 데이터를 object 형식으로 받아온다.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -70,8 +70,23 @@ export const startGithubLogin = (req, res) => {
   const finalUrl = `${baseUrl}?${params}`;
   return res.redirect(finalUrl);
 };
-export const finishGithubLogin = (req, res) => {
-  res.send("asd");
+export const finishGithubLogin = async (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/access_token";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    client_secret: process.env.GH_SECRET,
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  const data = await fetch(finalUrl, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  const json = await data.json();
+  console.log(json);
 };
 
 export const edit = (req, res) => res.send("Edit User");
