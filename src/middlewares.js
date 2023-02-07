9f7fd33ede7ff8448bab231675028b537a4eeaf6
wminsoo1 <wminsoo1@naver.com>
2023-02-07 21:02:53 +0900

    protector and public middlewares
    로그인 되어있는데 로그인 페이지를 들어간다던가 그런 현상을 방지하기 위해 middleware 제작

--- src/middlewares.js
+++ src/middlewares.js
@@ -4,3 +4,18 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.loggedInUser = req.session.user || {};
   next();
 };
+
+export const protectorMiddleware = (req, res, next) => {
+  if (req.session.user) {
+    return next();
+  } else {
+    return res.redirect("/login");
+  }
+};
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
+};
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -6,12 +6,17 @@ import {
   postlogin,
 } from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
+import { publicOnlyMiddleware } from "../middlewares";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
-rootRouter.route("/join").get(getjoin).post(postjoin);
-rootRouter.route("/login").get(getlogin).post(postlogin);
+rootRouter.route("/join").all(publicOnlyMiddleware).get(getjoin).post(postjoin);
+rootRouter
+  .route("/login")
+  .all(publicOnlyMiddleware)
+  .get(getlogin)
+  .post(postlogin);
 rootRouter.get("/search", search);
 
 export default rootRouter;
