68d1d462940ddaffd38ed98e71fc4d62c6b8cdbe
wminsoo1 <wminsoo1@naver.com>
2023-02-08 17:29:12 +0900

    change password part 2
    1. 비밀번호를 변경 후, 로그아웃이 되게 한다면,
    어차피 session은 destroy되어 session에 신경을 쓰지 않아도 된다.
    2. 비밀번호를 변경 후, 로그인을 계속 유지한다면,
    session은 업데이트 해주는 게 좋을 것 같다.
    언제 어디서 다시 session을 쓸지 모르기 때문이다.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,6 @@
 import User from "../models/User";
 import fetch from "node-fetch";
-import bcrypt from "bcrypt";
+import bcrypt, { compare } from "bcrypt";
 
 export const getjoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postjoin = async (req, res) => {
@@ -161,8 +161,30 @@ export const getChangePassword = (req, res) => {
   }
   res.render("users/change-password", { pageTitle: "Change Password" });
 };
-export const postChangePassword = (req, res) => {
-  return res.redirect("/");
+export const postChangePassword = async (req, res) => {
+  const {
+    session: {
+      user: { _id },
+    },
+    body: { oldPassword, newPassword, newPasswordConfirmation },
+  } = req;
+  const user = await User.findById(_id);
+  const ok = await bcrypt.compare(oldPassword, user.password);
+  if (!ok) {
+    return res.status(400).render("users/change-password", {
+      pageTitle: "Change Password",
+      errorMessage: "The current password is incorrect",
+    });
+  }
+  if (newPassword !== newPasswordConfirmation) {
+    return res.status(400).render("users/change-password", {
+      pageTitle: "Change Password",
+      errorMessage: "The password does not match the confirmation",
+    });
+  }
+  user.password = newPassword;
+  await user.save();
+  return res.redirect("/users/logout");
 };
 
 export const edit = (req, res) => res.send("Edit User");
