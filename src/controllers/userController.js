ddafb35ce80451f01a6b6e1928a9f7cd569fa112
wminsoo1 <wminsoo1@naver.com>
2023-02-09 20:36:47 +0900

    file uploads part 1
    Multer는 주로 파일 업로드에 사용되는 multipart/form-data를 처리하기 위한 node.js 미들웨어입니다.
    주의! Multer는 multipart(multipart/form-data)가 아닌 form을 처리하지 않습니다.
    multer(opts)
    dest 또는 storage: 파일을 저장할 위치
    fileFilter: 허용되는 파일을 제어하는 ​​함수
    limits: 업로드된 데이터의 한계
    preservePath: 기본 이름 대신 파일의 전체 경로 유지
    multer의 역할: input으로 avatar 파일을 받아서, 그 파일을 /uploads 폴더에 저장한 다음 그 파일 정보를 postEdit에게 전달해줌

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -140,7 +140,9 @@ export const postEdit = async (req, res) => {
       user: { _id },
     },
     body: { name, email, username, location },
+    file,
   } = req;
+  console.log(file);
   const updateUser = await User.findByIdAndUpdate(
     _id,
     {
