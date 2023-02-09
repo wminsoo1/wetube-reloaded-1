66eaf4d3e065a0cac14bdfaeb77d47a10e8b39c4
wminsoo1 <wminsoo1@naver.com>
2023-02-09 21:17:03 +0900

    file uploads part 2
    절대 DB에는 파일을 저장하지 않는다.
    대신 파일의 위치를 저장한다.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -137,7 +137,7 @@ export const getEdit = (req, res) => {
 export const postEdit = async (req, res) => {
   const {
     session: {
-      user: { _id },
+      user: { _id, avatarUrl },
     },
     body: { name, email, username, location },
     file,
@@ -146,6 +146,7 @@ export const postEdit = async (req, res) => {
   const updateUser = await User.findByIdAndUpdate(
     _id,
     {
+      avatarUrl: file ? file.path : avatarUrl,
       name,
       email,
       username,
--- src/models/User.js
+++ src/models/User.js
@@ -3,7 +3,7 @@ import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  avatar_url: String,
+  avatarUrl: String,
   socialOnly: { type: Boolean, default: false },
   username: { type: String, required: true, unique: true },
   password: { type: String },
